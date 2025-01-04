cb2401c156dfee79cc878b3fe2e856de1eb006b5
Thai Mai <jakehauer1234@gmail.com>
2025-01-04 22:05:36 +0700

    feat(post): implement CRUD functionality for post_service

--- backend/graphql/resolver.graphql.js
+++ backend/graphql/resolver.graphql.js
@@ -1,4 +1,5 @@
 import { authResolver } from "../services/auth_service/auth.resolver.js";
 import { userResolver } from "../services/user_service/user.resolver.js";
 import { friendResolver } from "../services/friend_service/friend.resolver.js";
-export const resolvers = [authResolver, userResolver, friendResolver];
+import { postResolver } from "../services/post_service/post.resolver.js";
+export const resolvers = [authResolver, userResolver, friendResolver, postResolver];
--- backend/graphql/typeDef.graphql.js
+++ backend/graphql/typeDef.graphql.js
@@ -1,4 +1,5 @@
 import { authTypeDef } from "../services/auth_service/auth.typeDef.js";
 import { userTypeDef } from "../services/user_service/user.typeDef.js";
 import { friendTypeDef } from "../services/friend_service/friend.typeDef.js";
-export const typeDefs = [authTypeDef, userTypeDef, friendTypeDef];
+import { postTypeDef } from "../services/post_service/post.typeDef.js";
+export const typeDefs = [authTypeDef, userTypeDef, friendTypeDef, postTypeDef];
