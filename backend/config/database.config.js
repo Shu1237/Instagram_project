4d6290c90a2b06bb6baf310a9f9e998f8eff69e8
Alien <minhhnghiii@gmail.com>
2025-08-01 11:58:09 +0700

    fix : db config !

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -21,7 +21,6 @@ const connect = async () => {
     await mongoose.connect(ENV_VARS.URI_MONGODB, {
       serverSelectionTimeoutMS: 30000, // 30 seconds
       socketTimeoutMS: 45000, // 45 seconds
-      bufferMaxEntries: 0, // Disable mongoose buffering
       maxPoolSize: 10,
       minPoolSize: 5,
       maxIdleTimeMS: 30000,
